432a0f378aea9c5f0dd2f240566ea02526524a75
Dolko42 <dolkodolkoo@gmail.com>
2024-02-22 12:47:36 +0100

    use optimistic for creating new list

--- src/_components/CreateList.tsx
+++ src/_components/CreateList.tsx
@@ -1,13 +1,40 @@
 import React from "react";
 import { createStufflist } from "~/app/api/todo/actions";
 
-const CreateList: React.FC = () => {
+type CreateListProps = {
+  addOptimisticList: (action: {
+    id: string;
+    title: string;
+    icon: string | null;
+    authorId: string;
+    _count: {
+      tasks: number;
+    };
+  }) => void;
+};
+
+const CreateList: React.FC<CreateListProps> = ({ addOptimisticList }) => {
   return (
-    <form action={createStufflist}>
+    <form
+      action={async () => {
+        addOptimisticList({
+          id: Math.random().toString(),
+          title: "Untitled list",
+          icon: null,
+          authorId: "authorId_here",
+          _count: {
+            tasks: 0,
+          },
+        });
+
+        await createStufflist();
+      }}
+    >
       <button className="w-full bg-lime-500 px-8 py-4 font-medium text-zinc-900">
         {`+`} New list
       </button>
     </form>
   );
 };
+
 export default CreateList;
--- src/app/todo/layout.tsx
+++ src/app/todo/layout.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Sidebar from "~/_components/Sidebar";
 import { db } from "~/server/db";
 
