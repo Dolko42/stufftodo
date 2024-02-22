e480fb1ff3caa14ff535e3131bb20e57c9a08f79
Dolko42 <dolkodolkoo@gmail.com>
2024-02-22 19:11:26 +0100

    some homepage button styling

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,11 +5,13 @@ export default async function Home() {
 
   return (
     <main className="relative flex h-full min-h-[40rem] flex-col bg-zinc-800 p-4">
-      <h1 className="pb-4 text-3xl text-white">
+      <h1 className="pb-6 text-3xl text-white">
         Welcome. You got stuff to do.
       </h1>
       <Link href="/todo">
-        <button className="btn btn-accent btn-lg">Get started {`->`}</button>
+        <button className="btn btn-lg w-full rounded-none bg-lime-500 text-zinc-800 hover:bg-lime-400 lg:w-auto">
+          Get started {`->`}
+        </button>
       </Link>
     </main>
   );
